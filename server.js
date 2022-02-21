9841a8894b4a8ecc15421d2cbb8aad2faad52b2f
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2022-02-21 15:37:04 +0100

    WIP secure api

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "files/");
@@ -15,11 +20,94 @@ const upload = multer({ storage: storage });
 const app = express();
 const port = 3000;
 
+let verifiedEmails = [];
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/confirmEmail", (req, res) => {
+  console.log("email", req.query.email);
+  console.log("token", req.query.token);
+  if (
+    verifiedEmails.some(
+      (el) =>
+        el.email === req.query.email &&
+        el.token === req.query.token &&
+        el.isVerified === false
+    )
+  ) {
+    verifiedEmails = verifiedEmails.map((el) => {
+      if (el.email === req.query.email) {
+        return {
+          ...el,
+          isVerified: true,
+        };
+      } else {
+        return el;
+      }
+    });
+    res.send("Your token is now valid: " + req.query.token);
+  }
+});
+
+app.get("/getToken", (req, res) => {
+  console.log(req.query.email);
+  const id = uuidv4();
+  if (verifiedEmails.some((el) => (el.email = req.query.email))) {
+    res.send("email already used");
+  } else {
+    verifiedEmails.push({ email: req.query.email, id: id, isVerified: false });
+    const msg = {
+      to: req.query.email, // Change to your recipient
+      from: "noreply@wildcodeschool.com", // Change to your verified sender
+      subject: "Confirm your email",
+      text:
+        "copy and paste this link in your browser http://localhost:3000/confirmEmail?email=" +
+        req.query.email +
+        "&token=" +
+        id,
+      html:
+        "<p>Click this link to confirm your email <a clicktracking='off' href=http://localhost:3000/confirmEmail?email=" +
+        req.query.email +
+        "&token=" +
+        id +
+        ">Clickme</a>",
+    };
+
+    sgMail
+      .send(msg)
+      .then((response) => {
+        console.log(response[0].statusCode);
+        console.log(response[0].headers);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    res.send("check you email to confirm");
+  }
+});
+
+app.get("/createToken", (req, res) => {
+  res.set("Content-Type", "text/html");
+  res.send(
+    Buffer.from(`
+      <form action="/getToken" method="get">
+      <div>
+        <label for="email">Enter your wildcodeschool email: </label>
+        <input type="text" name="email" id="email" required>
+      </div>
+      <div>
+        <input type="submit" value="Get Token!">
+      </div>
+    </form>`)
+  );
+});
+
 app.post("/upload", upload.single("fileData"), (req, res, next) => {
+  res.send("maintenance");
+  return;
   fs.readFile(req.file.path, (err, contents) => {
     if (err) {
       console.log("Error: ", err);
@@ -32,6 +120,8 @@ app.post("/upload", upload.single("fileData"), (req, res, next) => {
 });
 
 app.get("/list", (req, res) => {
+  res.send("maintenance");
+  return;
   fs.readdir("files", (err, files) => {
     if (err) {
       console.log(err);
@@ -45,7 +135,10 @@ app.get("/list", (req, res) => {
     }
   });
 });
+
 app.get("/files/:filename", (req, res) => {
+  res.send("maintenance");
+  return;
   let file = path.join(__dirname + "/files", req.params.filename);
   console.log("file", file);
   fs.readFile(file, (err, content) => {
